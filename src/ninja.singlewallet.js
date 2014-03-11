6165d059e060c35c7428274f04e8652c9c7c2362
Sean Hickey <sean@mincoin.io>
2014-03-10 23:38:34 -0400

    Initial import

---
+++ src/ninja.singlewallet.js
@@ -0,0 +1,39 @@
+ninja.wallets.singlewallet = {
+	open: function () {
+		if (document.getElementById("btcaddress").innerHTML == "") {
+			ninja.wallets.singlewallet.generateNewAddressAndKey();
+		}
+		document.getElementById("singlearea").style.display = "block";
+	},
+
+	close: function () {
+		document.getElementById("singlearea").style.display = "none";
+	},
+
+	// generate bitcoin address and private key and update information in the HTML
+	generateNewAddressAndKey: function () {
+		try {
+			var key = new Bitcoin.ECKey(false);
+			var bitcoinAddress = key.getBitcoinAddress();
+			var privateKeyWif = key.getBitcoinWalletImportFormat();
+			document.getElementById("btcaddress").innerHTML = bitcoinAddress;
+			document.getElementById("btcprivwif").innerHTML = privateKeyWif;
+			var keyValuePair = {
+				"qrcode_public": bitcoinAddress,
+				"qrcode_private": privateKeyWif
+			};
+			ninja.qrCode.showQrCode(keyValuePair, 4);
+
+            document.getElementById("qrcode_public").getElementsByTagName("canvas")[0].className = "single-qr-code";
+            document.getElementById("qrcode_private").getElementsByTagName("canvas")[0].className = "single-qr-code";
+		}
+		catch (e) {
+			// browser does not have sufficient JavaScript support to generate a bitcoin address
+			alert(e);
+			document.getElementById("btcaddress").innerHTML = "error";
+			document.getElementById("btcprivwif").innerHTML = "error";
+			document.getElementById("qrcode_public").innerHTML = "";
+			document.getElementById("qrcode_private").innerHTML = "";
+		}
+	}
+};
