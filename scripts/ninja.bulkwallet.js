33debc8a2489de772f1f0aa79fb0c0ea564a522b
Sean Hickey <sean@mincoin.io>
2014-03-11 05:14:47 -0400

    HTML refactoring

--- scripts/ninja.bulkwallet.js
+++ scripts/ninja.bulkwallet.js
@@ -4,7 +4,7 @@ ninja.wallets.bulkwallet = {
 		// show a default CSV list if the text area is empty
 		if (document.getElementById("bulktextarea").value == "") {
 			// return control of the thread to the browser to render the tab switch UI then build a default CSV list
-			setTimeout(function () { ninja.wallets.bulkwallet.buildCSV(3, 1, document.getElementById("bulkcompressed").checked); }, 200);
+			setTimeout(function () { ninja.wallets.bulkwallet.buildCSV(3, 1, false); }, 200);
 		}
 	},
 
